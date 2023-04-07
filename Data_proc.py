e35abaa7d568102cf65b253abd4c7a0c990607f5
anbharad <anil.bharadwaz@gmail.com>
2023-04-07 16:26:40 +0200

    corrected code

--- Data_proc.py
+++ Data_proc.py
@@ -1,12 +1,8 @@
-from re import A
-
-
 print('hari and Kedar are good boys')
-
 a = 5
 b = 6
 def sum(a,b):
-    c = a + b
-    return a
+    c = a+b
+    return c
+    print('this is correct'c)
 
-    print('wrong code'a)
